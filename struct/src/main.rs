f7193915a4e0f7e905213f66011b5899c4fc9191
Sébastien Gaïde <sgaide@roon.local>
2024-03-16 22:42:38 +0100

    add new languages, search and print a specific language

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,9 +1,11 @@
 #[derive(Debug)]
 enum Lang {
-  English,
-  Spanish,
-  Chinese,
-  Texan,
+    English,
+    Spanish,
+    Chinese,
+    Texan,
+    French,
+    German,
 }
 
 struct Greeting {
@@ -12,18 +14,47 @@ struct Greeting {
 }
 
 fn main() {
-  let mut v :Vec<Greeting> = Vec::new();
+    let mut v: Vec<Greeting> = Vec::new();
 
-  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
-  v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::English,
+        message: String::from("Hello WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Spanish,
+        message: String::from("Hola WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Texan,
+        message: String::from("Howdy WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::Chinese,
+        message: String::from("WasmEdge 你好!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::French,
+        message: String::from("Bonjour WasmEdge!"),
+    };
+    v.push(g);
+    let g: Greeting = Greeting {
+        lang: Lang::German,
+        message: String::from("guten tag WasmEdge!"),
+    };
+    v.push(g);
 
-  for e in v {
-    println!("{:?} {}", e.lang, e.message);
-  }
+    for (_, e) in v.iter().enumerate() {
+        println!("{:?} {}", e.lang, e.message);
+    }
+    match v
+        .iter()
+        .position(|greeting| matches!(greeting.lang, Lang::French))
+    {
+        Some(x) => println!("Found: {:?} {}", v[x].lang, v[x].message),
+        None => println!("{:?} Not found", Lang::French),
+    }
 }
